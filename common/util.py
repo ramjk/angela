0787c4dd6653326693a29435e084dc62dc70cb41
Alex Kazorian <akazorian@berkeley.edu>
2018-10-28 14:54:11 -0700

    Added test cases for (non-)membership. Failing verification

--- common/util.py
+++ common/util.py
@@ -6,3 +6,8 @@ FIXME: Is this good?
 class bitarray(bitarray):
     def __hash__(self):
         return self.tobytes().__hash__()
+
+def to_bytes(data) -> bytes:
+	if type(data) == bytes:
+		return data
+	return data.encode()
