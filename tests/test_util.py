787a8b7d0fdcf8f82431fcae5ebc094bb7e3f115
Cibi Pari <cibipari@gmail.com>
2018-10-28 19:55:51 -0700

    Added tests for batch insert

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,14 +1,14 @@
 import unittest
-from common import util
+from common.util import *
 
 class TestUtil(unittest.TestCase):
 	def test_to_bytes(self):
 		string = "this is a string"
-		bytestring = util.to_bytes(string)
+		bytestring = to_bytes(string)
 		self.assertEqual(bytes, type(bytestring))
 
 		bytestring = b"this is a bytestring"
-		bytestring = util.to_bytes(bytestring)
+		bytestring = to_bytes(bytestring)
 		self.assertEqual(bytes, type(bytestring))
 
 	def test_find_conflicts(self):
@@ -19,18 +19,16 @@ class TestUtil(unittest.TestCase):
 			bitarray([0, 1, 1, 0]),
 			bitarray([1, 1, 1, 0])
 				]
-
 		correct_conflict = {
 						bitarray(): True, 
 						bitarray([0, 0, 0]): True, 
 						bitarray([0, 0]): True,
 						bitarray([0]): True
 							}
-		conflicts = util.find_conflicts(leaves)
+		conflicts = find_conflicts(leaves)
 
 		self.assertEqual(len(conflicts), len(correct_conflict))
 		self.assertDictEqual(conflicts, correct_conflict)
-
 
 if __name__ == '__main__':
 	unittest.main()
