a87fd3e9c577aaecf7f79c84a4ae0f6e85c1b592
Alex Kazorian <akazorian@berkeley.edu>
2018-10-28 21:51:09 -0700

    Made insert benchmark pretty

--- benchmarks/merkle/benchmark_smt.py
+++ benchmarks/merkle/benchmark_smt.py
@@ -4,9 +4,12 @@ import timeit
 from common import util
 
 durations = list()
-iterations = list()
-for i in range(11):
+num_inserts = list()
+for i in range(6, 11):
 	durations.append(timeit.timeit('T.insert(util.random_index(), util.random_string())', setup='import merkle.smt; from common import util; T = merkle.smt.SparseMerkleTree("sha256")', number=2**i))
-	iterations.append(2**i)
-plt.plot(iterations, durations, "bo")
+	num_inserts.append(2**i)
+plt.plot(num_inserts, durations, "bo")
+plt.ylabel('Duration of random insert workload')
+plt.xlabel('Number of random inserts')
+plt.xticks([i for i in num_inserts], num_inserts)
 plt.show()
