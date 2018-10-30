24a358d41b4ee7d30540c99af33f13c4229b9aa4
Alex Kazorian <akazorian@berkeley.edu>
2018-10-30 11:13:45 -0700

    Added profiling for batch inserts

--- profiles/profile_smt.py
+++ profiles/profile_smt.py
@@ -6,6 +6,8 @@ values = [util.random_string() for i in range(10000)]
 proofs = list()
 T = merkle.smt.SparseMerkleTree("sha256")
 
+T_batch = merkle.smt.SparseMerkleTree("sha256")
+
 def profile_inserts():
 	pr = cProfile.Profile()
 
@@ -45,8 +47,20 @@ def profile_verifier():
 		p.sort_stats('time')
 		p.print_stats()	
 
+def profile_batch_inserts():
+	pr = cProfile.Profile()
+
+	pr.enable()
+	T_batch.batch_insert({k:v for (k,v) in zip(keys, values)})
+	pr.disable()
+
+	with open("profiles/profile_batch_inserts.txt", 'w') as output:
+		p = pstats.Stats(pr, stream=output)
+		p.sort_stats('time')
+		p.print_stats()
+
 if __name__ == '__main__':
 	profile_inserts()
 	profile_prover()
 	profile_verifier()
-	
+	profile_batch_inserts()
