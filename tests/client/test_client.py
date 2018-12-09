df7809e7e881076105be30f9af58ca2f50e01d23
Alex Kazorian <akazorian@berkeley.edu>
2018-12-09 13:00:40 -0800

    Fixed test_client

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -1,6 +1,7 @@
 import unittest
 import threading
 import ray
+import time
 
 from common import util
 from server.transaction import WriteTransaction
@@ -8,53 +9,23 @@ from client.client import Client
 from server.server import Server
 
 class TestSparseMerkleTree(unittest.TestCase):
-	def setUp(self):
-		port = 3307
-		num_worker = 9
-		epoch_length = 1
-		tree_depth = 256
-
-		# root = Client.get_signed_root()
-	
-		self.server = Server(port, num_worker, epoch_length, tree_depth, True)
-		self.server_thread = threading.Thread(target=self.server.start)
-		self.client = Client("localhost", port)
-
-		for i in range(4):
-			print()
-		print("__________________________________________")
-
-	def tearDown(self):
-		self.server.close()
-		self.client.end_session()
 
 	def test_practice(self):
-		self.server_thread.start()
-		index = util.random_index()
-		data = util.random_string()
-		self.client.insert_leaf(index, data)
-		print("Getting signed root")
-		# root = self.client.get_signed_root()
-		print("Generating proof")
-		proof = self.client.generate_proof(index)
-		print("root", root)
-		self.assertTrue(self.client.verify_proof(proof, data, root))
+		indices = [util.random_index() for i in range(128)]
+		data = [util.random_string() for i in range(128)]
 
-		# index = util.random_index()
-		# data = util.random_string()
-		# for i in range(1):
-		# 	index = util.random_index()
-		# 	data = util.random_string()
-		# 	tx = Client.insert_leaf(index, data)
-		# tx = Client.insert_leaf(index, data)
-		# root = Client.get_signed_root()
-		# proof = Client.generate_proof(index)
-		# self.assertTrue(Client.verify_proof(proof, data, root))
+		print("Inserting")
+		for i in range(128):
+			Client.insert_leaf(indices[i], data[i])
 
-		# index = util.random_index()
-		# data = util.random_string()
-		# root = Client.get_signed_root()
-		# self.assertTrue(Client.verify_proof(Client.generate_proof(index), data, root))
+		print("Getting root")
+		root = Client.get_signed_root()
+
+		print("Proving")
+		for i in range(128):
+			proof = Client.generate_proof(indices[i])
+			d = data[i]
+			self.assertTrue(Client.verify_proof(proof, d, root))
 
 if __name__ == '__main__':
 	unittest.main()
